92b10ee829fca7614a65ec8dd66873f472854752
Daniel Peter <daniel.peter@me.com>
2021-01-14 17:39:37 +0100

    Make sure deeplinks are member-initializable internally

--- Sources/ComposableNavigator/Deeplink/Deeplink.swift
+++ Sources/ComposableNavigator/Deeplink/Deeplink.swift
@@ -2,8 +2,10 @@ import Foundation
 
 public struct Deeplink {
   public let components: [DeeplinkComponent]
+}
 
-  public init?(url: URL, matching scheme: String) {
+public extension Deeplink {
+  init?(url: URL, matching scheme: String) {
     guard url.scheme == scheme else {
       return nil
     }
