fbd936bf6ac944b3622e3e25411b00ea39e272b5
Daniel Peter <daniel.peter@me.com>
2021-01-27 17:54:06 +0100

    fixup! Adds beforeBuild helper

--- Sources/ComposableNavigator/PathBuilder/PathBuilders/PathBuilders+BeforeBuild.swift
+++ Sources/ComposableNavigator/PathBuilder/PathBuilders/PathBuilders+BeforeBuild.swift
@@ -1,4 +1,5 @@
 public extension PathBuilder {
+    /// Performs an action before this path builder is built.
     func beforeBuild(perform action: @escaping () -> Void) -> _PathBuilder<Content> {
         _PathBuilder(
             buildPath: { path in
