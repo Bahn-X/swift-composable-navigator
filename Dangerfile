d2fcfb6b98f2e44844bb60a534ccf1413ce66993
Daniel Peter <daniel.peter@me.com>
2021-02-17 11:04:19 +0100

    Add dangerfile

---
+++ Dangerfile
@@ -0,0 +1,5 @@
+xcov.report(
+   scheme: 'ComposableNavigator',
+   workspace: 'ComposableNavigator.xcworkspace',
+   exclude_targets: ['Example.app', 'ComposableNavigatorTests.xctest', 'ComposableDeeplinkingTests.xctest']
+)
--- Dangerfile.swift
+++
@@ -1,9 +0,0 @@
-import Danger
-import DangerSwiftCoverage  // package: https://github.com/f-meloni/danger-swift-coverage.git
-
-let danger = Danger()
-
-Coverage.xcodeBuildCoverage(
-  .derivedDataFolder("Build"),
-  excludedTargets: ["ComposableNavigatorTests.xctest", "ComposableDeeplinkingTests.xctest"]
-)
