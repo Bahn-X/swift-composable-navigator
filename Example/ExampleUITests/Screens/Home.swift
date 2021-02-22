d7dfd918a18f4d3d73fee93704d95fe3194325a4
Daniel Peter <daniel.peter@me.com>
2021-02-22 15:07:51 +0100

    Fix broken file reference

---
+++ Example/ExampleUITests/Screens/Home.swift
@@ -0,0 +1,42 @@
+@testable import Example
+import XCTest
+
+class Home: Base {
+  lazy var settingsButton = app
+    .buttons[AccessibilityIdentifier.HomeScreen.settingsNavigationBarItem]
+    .await()
+
+  func detail(for id: String) -> XCUIElement {
+    app.buttons[AccessibilityIdentifier.HomeScreen.detail(for: id)]
+      .await()
+  }
+
+  func detailSettings(for id: String) -> XCUIElement {
+    app.buttons[AccessibilityIdentifier.HomeScreen.detailSettings(for: id)]
+      .await()
+  }
+
+  @discardableResult
+  func assertVisible() -> Self {
+    XCTAssertTrue(settingsButton.exists)
+    return self
+  }
+
+  @discardableResult
+  func goToSettings() -> Settings<Home> {
+    settingsButton.tap()
+    return Settings(predecessor: self, prefix: "home", app: app)
+  }
+
+  @discardableResult
+  func goToDetail(for id: String) -> Detail {
+    detail(for: id).tap()
+    return Detail(app: app, id: id)
+  }
+
+  @discardableResult
+  func goToDetailSettings(for id: String) -> Detail {
+    detail(for: id).tap()
+    return Detail(app: app, id: id)
+  }
+}
